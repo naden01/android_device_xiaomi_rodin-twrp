ec383af7cc6c9e69abbeb70b09b6edbfa962394c
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-10-13 06:03:46 -0700

    test again

--- recovery/root/system/bin/load-touch-module.sh
+++ recovery/root/system/bin/load-touch-module.sh
@@ -1,13 +1,14 @@
-#!/sbin/sh
 set +e
-mod_dir="/lib/modules"
+mod_dir="/vendor_dlkm/lib/modules"
 modules=(
+  xiaomi_touch_rodin.ko
+  scp.ko
   focaltech_touch_rodin.ko
 )
 
+mount /vendor_dlkm
 # load modules
 for module in "${modules[@]}"; do
   insmod $mod_dir/$module
 done
-
-exit 0
+umount /vendor_dlkm
